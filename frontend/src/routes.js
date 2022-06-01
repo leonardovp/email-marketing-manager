49d13a300b723cfe56b0f09d8930253d7b422582
Leonardo Versiani <leonardovp@gmail.com>
2022-06-01 15:35:32 -0300

    Make routes react

---
+++ frontend/src/routes.js
@@ -0,0 +1,144 @@
+import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom'
+
+function Home(){
+  return(
+    <div>
+      <Menu /> 
+      <h2>Dasboard</h2>
+    </div>
+  )
+}
+
+function Contacts(){ 
+
+  return(
+    <div>
+      <Menu /> 
+      <h2>Contatos</h2>
+      <ul>
+        <li>
+        <Link to="/contacts/1">Contato A</Link> 
+        </li>
+        <li>        
+          <Link to="/contacts/2">Contato B</Link>         
+        </li>
+        <li>         
+          <Link to="/contacts/3">Contato C</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path=":contactId" element={<Contact />} />
+      </Routes>
+
+    </div>
+
+
+
+
+  )
+}
+
+function Contact(){
+
+  let { contactId } = useParams();
+
+  return(
+
+      <div>
+        <h3>Contato ID: {contactId}</h3>
+      </div>
+
+  )
+
+}
+
+function Signin(){
+  return(
+    <div>
+      <h2>Login</h2>
+    </div>
+  )
+}
+
+function Signup(){
+  return(
+    <div>     
+      <h2>Cadastro</h2>
+    </div>
+  )
+}
+
+function Messages(){ 
+
+  return(
+    <div>
+      <Menu /> 
+      <h2>Mensagens</h2>
+      <ul>
+        <li>
+        <Link to="/messages/1">Mensagen A</Link> 
+        </li>
+        <li>        
+          <Link to="/messages/2">Mensagen B</Link>         
+        </li>
+        <li>         
+          <Link to="/messages/3">Mensagen C</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path=":messageId" element={<Message />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+function Message(){
+
+  let { messageId } = useParams();
+
+  return(
+    <div>
+      <h3>Mensagem ID: {messageId}</h3>
+  </div>
+  )
+}
+
+
+function Menu() {
+  return (
+    <div>     
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/contacts">Contatos</Link>
+        <Link to="/messages">Mensagens</Link>
+        <Link to="/signin">Sair</Link>       
+      </nav>     
+    </div>
+  );
+}
+
+export default function Rotas(){
+  return(
+      <BrowserRouter>
+      <Routes>     
+        <Route index element={<Home />} />
+        <Route path='contacts/*' element={<Contacts />} />
+        <Route path='messages/*' element={<Messages />} />
+        <Route path='signin' element={<Signin />} />
+        <Route path='signup' element={<Signup />} />
+      </Routes>
+      </BrowserRouter>
+    
+ 
+  )
+}
