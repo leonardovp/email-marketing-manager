6203707ed3c81f47de55f9d2c75ab395710c716b
Leonardo Versiani <leonardovp@gmail.com>
2022-06-09 09:41:53 -0300

    routes secure

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link,
+    useParams,
+  } from 'react-router-dom';
+
+import SignInPage from "../pages/public/SignIn";
+import SignUpPage from "../pages/public/SignUp";
+import Dashboard from "../pages/secure/Dashboard/Dashboard"; 
+
---
+++ frontend/src/routes/routes-wrapper.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    Route
+  } from "react-router-dom";
+import Auth from "../services/auth";
+
+const RouteWrapper = ({component: Component, ...rest}) => {
+
+
+    return(
+
+        <Route /> 
+
+
+    )
+
+
+}
