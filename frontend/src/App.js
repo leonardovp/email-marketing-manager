17489249da7a09c6076c6dd8c29e1866fde340b0
Leonardo Versiani <leonardovp@gmail.com>
2022-06-09 12:20:39 -0300

    login secure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Header from './pages/public/__Common__/header';
 import RoutesApp from './routes/index'
 import { BrowserRouter } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
-import Alert from './pages/public/__Common__/alert';
 
 const App = () => {  
 
--- frontend/src/routes/verify-login-routes.js
+++ frontend/src/routes/verify-login-routes.js
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import {
-    Route, useNavigate, Navigate, Outlet 
+    Navigate, Outlet 
   } from "react-router-dom";
 import Auth from "../services/auth";
 
 const VerifyRoutes = () => {
-
-    let navigate = useNavigate();
 
     return(
 
